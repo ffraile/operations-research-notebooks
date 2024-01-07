f0070b51dde0b7febace2e93b7c126cd768faa55
Francisco <ffraile@cigip.upv.es>
2024-01-07 13:01:04 +0100

    - Updated theme to furo - Updated logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,19 +18,24 @@
 # -- Project information -----------------------------------------------------
 
 project = 'operations_research_notebooks'
-copyright = '2020, Francisco Fraile'
+copyright = '2023, Francisco Fraile'
 author = 'Francisco Fraile'
 
 # The full version, including alpha/beta/rc tags
-release = 'v2'
+release = 'v3'
 
+# The logo
+html_logo = '_static/logo.png'
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['nbsphinx']
+extensions = ['nbsphinx', 'myst_parser', 'sphinx_design', 'sphinx_copybutton', 'sphinxext.opengraph']
+
+# Add myst extensions to enable admonitions
+myst_enable_extensions = ["colon_fence", "html_admonition"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -40,13 +45,17 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Source suffixes
+source_suffix = {
+    '.md': 'markdown'
+}
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'bizstyle'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -55,3 +64,8 @@ html_theme = 'bizstyle'
 
 nbsphinx_execute = 'never'
 
+## Latex engine to support unicode characters
+latex_engine = "lualatex"
+latex_elements = {
+    "preamble": "\\usepackage{svg}\n",
+}
