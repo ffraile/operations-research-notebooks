dbbf60fad8d68b5187354639ae93087bda959205
Francisco <ffraile@cigip.upv.es>
2024-10-23 17:32:28 +0200

    review fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,10 +67,31 @@ nbsphinx_execute = 'never'
 
 ## Latex engine to support unicode characters
 latex_engine = "lualatex"
-latex_elements = {
-    "preamble": "\\usepackage{svg}\n"
-}
 
+
+latex_elements = {
+    'preamble': r'''
+        \usepackage{svg}
+        \usepackage{titlesec}
+        \usepackage{xcolor}
+        \usepackage[normalem]{ulem}  % For underline (without wavy effect)
+
+        % Define color for headings (same as level 3 heading color)
+        \definecolor{level3color}{RGB}{51, 71, 140}  % Adjust this RGB to your exact color
+
+        % Customize level 4 (\subsubsection) - Same font size and color as level 3, underlined and italic
+        \titleformat{\subsubsection}
+            {\normalfont\itshape\underline\color{level3color}\large}{\thesubsubsection}{1em}{}
+
+        % Customize level 5 (\paragraph) - Same color and size as paragraph text, underlined
+        \titleformat{\paragraph}
+            {\normalfont\underline\normalsize\color{black}}{\theparagraph}{1em}{}
+
+        % Optional: Control spacing around the headings
+        \titlespacing*{\subsubsection}{0pt}{1.5ex plus .1ex minus .2ex}{1ex}
+        \titlespacing*{\paragraph}{0pt}{1.5ex plus .1ex minus .2ex}{1ex}
+    '''
+}
 
 root_doc = 'index'
 latex_documents = [
